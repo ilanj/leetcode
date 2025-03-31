6b13a94d730cb037be20fc21ff2894461a990fc1
Ilanchezhian <ilanchezhian619@gmail.com>
2025-03-31 16:33:27 +0530

    Update 2_linkedlist_add2nos.py
    updated to run in local

--- 50/2_linkedlist_add2nos.py
+++ 50/2_linkedlist_add2nos.py
@@ -26,3 +26,52 @@ The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
 """
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def create_linked_list(lst):
+    dummy = ListNode(0)
+    curr = dummy
+    for num in lst:
+        curr.next = ListNode(num)
+        curr = curr.next
+    return dummy.next
+
+# Helper function to print a linked list
+def print_linked_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    print(result)
+
+# Create linked lists
+l1 = create_linked_list([2,4,3])  # Represents 342
+l2 = create_linked_list([5,6,4])  # Represents 465
+
+# only below class wo function calls to submit with leetcode
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = curr.next
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
+
+run = Solution()
+res = run.addTwoNumbers(l1, l2)
+print_linked_list(res)
